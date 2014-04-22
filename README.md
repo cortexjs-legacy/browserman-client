1e11b602274d9b26092927fc606c5b2261824fff
ltebean <yucong1118@gmail.com>
2014-04-22 13:58:39 +0800

    add verbose result

---
+++ README.md
@@ -0,0 +1,70 @@
+###Usage:
+
+	Usage: browserman [options] [command]
+
+	Commands:
+
+		config [options]       config server port and address
+		list                   list all available browsers
+		test [options]         test a page in all browsers that meets the requirement
+		
+###List available browsers:
+
+run:	
+	
+	browserman list
+	
+and you would find the browsers that are ready to work:
+
+	-------------------------------------------
+	3 workers found
+	-------------------------------------------
+	Firefox(28.0.0)
+	Safari(6.0.0)
+	Chrome(34.0.0)
+	
+###Run a test:
+
+the usage:
+
+	Usage: test [options]
+		
+		Options:
+		
+			-d, --destination [destination]  the destination page url to test
+			-b, --browser [browser]          browser name
+			-v, --version [version]          browser version, for example: >2.0
+			-V, --verbose [version]          verbose or not
+
+for example, run: 
+
+	browserman test --destination='http://localhost:9000/public/test.html' 
+	
+you will see:
+	
+	✓ passes: 3 failures: 0 on Safari(6.0)
+	✓ passes: 3 failures: 0 on Chrome(34.0)
+	✓ passes: 3 failures: 0 on Firefox(28.0)
+	
+and you can use --verbose to see the detail:
+
+	-----------------------------------------------------------
+	✓ passes: 3 failures: 0 on Safari(6.0)
+	-----------------------------------------------------------
+	✓ Foobar #convertText() should convert text
+	✓ Foobar #runMethod() should run a method
+	✓ Foobar #sayHello() should return some text
+
+	-----------------------------------------------------------
+	✓ passes: 3 failures: 0 on Chrome(34.0)
+	-----------------------------------------------------------
+	✓ Foobar #convertText() should convert text
+	✓ Foobar #runMethod() should run a method
+	✓ Foobar #sayHello() should return some text
+
+	-----------------------------------------------------------
+	✓ passes: 3 failures: 0 on Firefox(28.0)
+	-----------------------------------------------------------
+	✓ Foobar #convertText() should convert text
+	✓ Foobar #runMethod() should run a method
+	✓ Foobar #sayHello() should return some text
--- bin/test.js
+++ bin/test.js
@@ -1,35 +1,52 @@
 var Browserman = require('../lib/browserman');
 var logger = require('../lib/logger');
-var config=require('../lib/config');
-var colors=require('colors');
+var config = require('../lib/config');
+var colors = require('colors');
 
 module.exports.execute = function(options) {
 	var browserman = new Browserman(config.load());
-	browserman.test(options,
-		function(err, results) {
-			for (var i = results.length - 1; i >= 0; i--) {
-				printTestResult(results[i])
-			}
-			browserman.exit();
+	browserman.test(options, function(err, results) {
+		if (options.verbose) {
+			printVerboseTestResult(results)
+
+		} else {
+			printTestResult(results)
 		}
-	)
+
+		browserman.exit();
+	})
 }
 
-function printTestResult(result) {
-	var browser = result.browser;
-	var data = result.data;
-	console.log('-----------------------------------------------------------');
-	console.log('passes: %d failures: %d on %s(%s)', data.passes.length, data.failures.length, browser.name, browser.version);
-	console.log('-----------------------------------------------------------');
+function printTestResult(results) {
+	for (var i = results.length - 1; i >= 0; i--) {
+		var result = results[i]
+		var browser = result.browser;
+		var data = result.data;
+		var prefix=data.failures.length==0? '\u2713'.green : '\u2717'.red;
+		console.log('%s passes: %d failures: %d on %s(%s)',prefix, data.passes.length, data.failures.length, browser.name, browser.version);
+	}
 
-	for (var i = data.passes.length - 1; i >= 0; i--) {
-		console.log('\u2713 '.green + data.passes[i].fullTitle)
-	};
-	for (var i = data.failures.length - 1; i >= 0; i--) {
-		console.log('\u2717 '.red + data.failures[i].fullTitle)
-		console.log('	'+ data.failures[i].error.red)
+}
 
-	};
-	console.log('');
+function printVerboseTestResult(results) {
+	for (var i = results.length - 1; i >= 0; i--) {
+		var result = results[i]
+		var browser = result.browser;
+		var data = result.data;
+		var prefix=data.failures.length==0? '\u2713'.green : '\u2717'.red;
+		console.log('-----------------------------------------------------------');
+		console.log('%s passes: %d failures: %d on %s(%s)',prefix, data.passes.length, data.failures.length, browser.name, browser.version);
+		console.log('-----------------------------------------------------------');
+
+		for (var j = data.passes.length - 1; j >= 0; j--) {
+			console.log('\u2713 '.green + data.passes[j].fullTitle)
+		};
+		for (var j = data.failures.length - 1; j >= 0; j--) {
+			console.log('\u2717 '.red + data.failures[j].fullTitle)
+			console.log('	' + data.failures[j].error.red)
+
+		};
+		console.log('');
+	}
 
 }
