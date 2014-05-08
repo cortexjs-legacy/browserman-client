533d5d3434e9015137f2cf9a416f85d3189eaa50
ltebean <yucong1118@gmail.com>
2014-05-08 16:00:42 +0800

    add reporter option

--- bin/test.js
+++ bin/test.js
@@ -1,67 +1,12 @@
 var Browserman = require('../lib/browserman');
 var logger = require('../lib/logger');
 var config = require('../lib/config');
-var colors = require('colors');
-var cp = require('child_process');
-var sf = require('string-format');
-var fs=require('fs');
 
 module.exports.execute = function(options) {
 
 	var browserman = new Browserman(config.load());
 	var test = browserman.test(options);
-	test.on('done', function(result) {
-		if (options.verbose) {
-			printVerboseResult(result)
-		} else {
-			printResult(result)
-		}
-		if (result.screenshot) {
-			var browser = result.browser;
-			saveScreenshot({
-				screenshot: result.screenshot,
-				title: '{name}({version}-{os})'.format(browser)
-			});
-		}
-	}).on('error', function(err) {
-		console.log(err.message);
-	}).on('complete', function() {
-		process.exit(0);
-	})
-}
-
-function isValidUrl(url) {
-	return /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url);
-}
-
-function printResult(result) {
-	var browser = result.browser;
-	var data = result.data;
-	var prefix = data.failures.length == 0 ? '\u2713'.green : '\u2717'.red;
-	console.log('%s passes: %d failures: %d on %s %s / %s', prefix, data.passes.length, data.failures.length, browser.name, browser.version, browser.os);
-}
-
-function printVerboseResult(result) {
-	var browser = result.browser;
-	var data = result.data;
-	var prefix = data.failures.length == 0 ? '\u2713'.green : '\u2717'.red;
-	console.log('-----------------------------------------------------------'.rainbow);
-	console.log('%s passes: %d failures: %d on %s %s / %s', prefix, data.passes.length, data.failures.length, browser.name, browser.version, browser.os);
-	console.log('-----------------------------------------------------------'.rainbow);
-	for (var j = data.passes.length - 1; j >= 0; j--) {
-		console.log('\u2713 '.green + data.passes[j].fullTitle)
-	};
-	for (var j = data.failures.length - 1; j >= 0; j--) {
-		console.log('\u2717 '.red + data.failures[j].fullTitle)
-		console.log('  ' + data.failures[j].err.stack.red)
-	};
-	console.log('');
-}
-
-function saveScreenshot(options) {
-	var html = '<html><body>' + options.screenshot + '</body></html>';
-	var path = config.home() + '/' + options.title + '.html';
-	fs.writeFileSync(path, html);
-	console.log('  save screenshot to: %s', path);
-	console.log('');
+	require('../lib/reporter/' + options.reporter).report(test,{
+		verbose:options.verbose
+	});
 }
